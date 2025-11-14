288ecf9ec6889fe6b63426c85ca0b608c25e6588
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-11-13 18:43:20 -0600

    TanStack Query - Requests and cache

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,17 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+import { getPokemons } from '../../../actions/pokemons';
 
 export const HomeScreen = () => {
+
+  const { isLoading, data } = useQuery({
+    queryKey: [], // identificador para poder manejarlo en caché
+    queryFn: () => getPokemons(), // promesa 
+    staleTime: 100 * 60 * 60, // tiempo que va a mantener fresca la respuesta de la petición 
+  });
+
   return (
     <View>
         <Text variant='displayMedium'>HomeScreen</Text>
-        <Button
-          mode="contained"
-          onPress={() => console.log('Pressed')}
-        >
-          Press me
-        </Button>
+
+        { 
+          isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <Button
+              mode="contained"
+              onPress={() => console.log('Pressed')}
+            >
+              Press me
+            </Button>
+          )
+        }
+
     </View>
   );
 };

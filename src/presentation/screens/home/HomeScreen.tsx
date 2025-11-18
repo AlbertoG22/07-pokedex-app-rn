052f8698314b1a4aa93d37b85f566ef1e95db7ec
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-11-18 14:27:37 -0600

    HomeScreen design

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,34 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
+import { PokeballBg } from '../../components/ui/PokeballBg';
 
 export const HomeScreen = () => {
 
-  const { isLoading, data } = useQuery({
+  const { isLoading, data = [] } = useQuery({
     queryKey: [], // identificador para poder manejarlo en caché
-    queryFn: () => getPokemons(), // promesa 
+    queryFn: () => getPokemons(0), // promesa 
     staleTime: 100 * 60 * 60, // tiempo que va a mantener fresca la respuesta de la petición 
   });
 
   return (
     <View>
-        <Text variant='displayMedium'>HomeScreen</Text>
 
-        { 
-          isLoading ? (
-            <ActivityIndicator />
-          ) : (
-            <Button
-              mode="contained"
-              onPress={() => console.log('Pressed')}
-            >
-              Press me
-            </Button>
-          )
-        }
+      <PokeballBg style={ styles.imgPosition } />
 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+    right: -100,
+  }
+});
